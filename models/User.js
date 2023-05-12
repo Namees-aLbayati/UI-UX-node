d0d205dd1aba88cd93b45745cfece13638ebd935
Namees Albayati <nameesalbayati@fb.com>
2023-05-12 16:45:57 -0500

    add models

---
+++ models/User.js
@@ -0,0 +1,36 @@
+const sequelize=require('../config/connection')
+const {Model,DataTypes}=require('sequelize');
+class User extends Model{};
+User.init({
+    id:{
+        type:DataTypes.INTEGER,
+        allowNull:false,
+        primaryKey:true,
+        autoIncrement:true
+    },
+    username:{
+        type:DataTypes.STRING
+    },
+    email:{
+        type:DataTypes.STRING,
+      unique:true,
+        validate:{
+            isEmail:true
+        }
+    },
+    password:{
+        type:DataTypes.STRING,
+        validate:{
+            len:[8]
+        }
+    },
+    isAdmin:{
+        type:DataTypes.BOOLEAN
+    }
+},{
+    sequelize,
+    freezeTableName:true});
+
+   
+
+    module.exports=User;
